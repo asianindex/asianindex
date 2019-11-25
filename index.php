9fd43016394227ed815ad70d18c5081692e57ea2
Asian Index <58131226+asianindex@users.noreply.github.com>
2019-11-25 13:07:25 +1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+
+<html lang='en'>
+  <head>
+    <style type="text/css">
+.auto-style1 {
+	font-family: "Franklin Gothic Heavy";
+}
+.auto-style4 {
+	margin-top: 0px;
+}
+.auto-style5 {
+	text-decoration: underline;
+	font-size: large;
+}
+.auto-style7 {
+	background-image: url('src/white%20sausage.jpg');
+	background-color: #FFFFFF;
+}
+.auto-style8 {
+	height: 2px;
+	color: #000000;
+	margin-left: 2px;
+	margin-right: 2px;
+}
+</style>
+    <div id="Title">
+      <title>Asian Index</title>
+      <title>Your Number 1 Information Source about Asia..</title>
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
+		<h1 class="auto-style1">
+		<center><img class="auto-style4" height="49" src="Afghanistan%20Wave.gif" width="90"><b>Asian Index<b><img height="49" src="Yemen%20Wave.gif" width="90"></center</h1><span lang="en-gb">&nbsp;
+		</span> <a target='_blank' a href="https://github.com/asianindex">
+		<img height="31" src="Github-icon.png" width="38"></a>
+        <hr class="auto-style8" noshade="noshade">
+		<noscript><center>This Website Requires Javascript To Run, Please Enable Javascript Or Go On To A Browser That Supports Javascript</center></noscript>
+		<h5><b>The largest, tallest and most populated continent in the world.</b></h5>
+	  <h5>Click on a country to learn more</h5>
+    </div>
+  </head>
+  <body class="auto-style7">
+    <div>
+<div class="container list-container">
+<div><center>The list of Asian countries is in alphabetical orders</center></div>
+<div>
+<ul class="partA-J"> 
+<center><b>A</b>
+<a target='_blank' href='Afghanistan.html'>Afghanistan</a>,
+<a target='_blank' href='Armenia.html?i=1'>Armenia</a>,
+<a target='_blank' href='Azerbaijan.html'>Azerbaijan</a>
+<br>
+<b>B</b>
+<a target='_blank' href='Bahrain.html'>Bahrain</a>,
+<a target='_blank' href='Bangladesh.html'>Bangladesh</a>,
+<a target='_blank' href='Bhutan.html'>Bhutan</a>,
+<a target='_blank' href='Brunei.html'>Brunei</a>
+<br>
+<b>C</b>
+<a target='_blank' href='Cambodia.html'>Cambodia</a>,
+<a target='_blank' href='China.html'>China</a>,
+<a target='_blank' href='Cyprus.html'>Cyprus</a>
+<br>
+<b>G</b>
+<a target='_blank' href='Georgia.html'>Georgia</a>
+<br>
+<b>I</b>
+<a target='_blank' href='India.html'>India</a>,
+<a target='_blank' href='Indonesia.html'>Indonesia</a>,
+<a target='_blank' href='Iran.html'>Iran</a>,
+<a target='_blank' href='Iraq.html'>Iraq</a>,
+<a target='_blank' href='Israel.html'>Israel</a>
+<br>
+<b>J</b>
+<a target='_blank' href='Japan.html'>Japan</a>,
+<a target='_blank' href='Jordan.html'>Jordan</a>
+<br>
+<b>K</b>
+<a target='_blank' href='Kazakhstan.html'>Kazakhstan</a>,
+<a target='_blank' href='Kuwait.html'>Kuwait</a>,
+<a target='_blank' href='Kyrgyzstan.html'>Kyrgyzstan</a>
+<br>
+<b>L</b>
+<a target='_blank' href='Laos.html'>Laos</a>,
+<a target='_blank' href='Lebanon.html'>Lebanon</a>
+<br>
+<b>M</b>
+<a target='_blank' href='Malaysia.html'>Malaysia</a>,
+<a target='_blank' href='Maldives.html'>Maldives</a>,
+<a target='_blank' href='Mongolia.html'>Mongolia</a>,
+<a target='_blank' href='Myanmar.html'>Myanmar <span class="black">(formerly Burma)</span></a>
+<br>
+<b>N</b>
+<a target='_blank' href='Nepal.html'>Nepal</a>,
+<a target='_blank' href='North-Korea.html'>North Korea</a>&nbsp;&nbsp;<b>O</b>
+<a target='_blank' href='Oman.html'>Oman</a>
+<br>
+<b>P</b>
+<a target='_blank' href='Pakistan.html'>Pakistan</a>,
+<a target='_blank' href='Palestine.html'>Palestine</a>,
+<a target='_blank' href='Philippines.html'>Philippines</a>
+<br>
+<b>Q</b>
+<a target='_blank' href='Qatar.html'>Qatar</a>&nbsp;&nbsp;<b>R</b>
+<a target='_blank' href='Russia.html'>Russia</a>
+<br>
+<b>S</b>
+<a target='_blank' href='Saudi-Arabia.html'>Saudi Arabia</a>,
+<a target='_blank' href='Singapore.html'>Singapore</a>,
+<a target='_blank' href='South-Korea.html'>South Korea</a>,
+<a target='_blank' href='Sri-Lanka.html'>Sri Lanka</a>,
+<a target='_blank' href='Syria.html'>Syria</a>
+<br>
+<b>T</b>
+<a target='_blank' href='Taiwan.html'>Taiwan</a>,
+<a target='_blank' href='Tajikistan.html'>Tajikistan</a>,
+<a target='_blank' href='Thailand.html'>Thailand</a>,
+<a target='_blank' href='Timor-Leste.html'>Timor-Leste</a>,
+<a target='_blank' href='Turkey.html'>Turkey</a>,
+<a target='_blank' href='Turkmenistan.html'>Turkmenistan</a>
+<br>
+<b>U</b>  
+<a target='_blank' href='United-Arab-Emirates.html'>United Arab Emirates <span class="black">(UAE)</span></a>,
+<a target='_blank' href='Uzbekistan.html'>Uzbekistan</a>
+<br>
+<b>V</b>
+<a target='_blank' href='Vietnam.html'>Vietnam</a>&nbsp;&nbsp;<b>Y</b>
+<a target='_blank' href='Yemen.html'>Yemen</a></center>
+</ul>
+  <hr>
+    </div>
+    <div id='Info'>
+    <center><p class='information'>This Website may have outdated information, for more information click <a target='_blank' href='information.html'>here.</a>
+    	<p class='auto-style5'><strong><em>&#169;2019 Continent-Index LTD, All Rights Reserved</em></strong></div></center>
+		<marquee behavior="scroll" direction="right">William And Dan's Continent Index </marquee>
+		</div>
+	</b></b>
+	<form method="post">
+		<center><button name="Abutton1">Refresh Page / Back to Top</button></center>
+	</form>
+    </div>
+	<center><a target='_blank' href='updates.html'>Updates</a><center>
+        <center> <a href='https://www.stat-counter.org/'>Hits!</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=b6b365e0ddc602370479b87925eedd430b4755c7'></script>
+<script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/603949/t/7"></script><center>
+  </body>
+</html>
